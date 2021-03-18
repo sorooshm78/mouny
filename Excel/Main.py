7f4dd08efaea6114b796d7f743002821c0c917d6
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-03-18 20:21:32 +0330

    .

--- Excel/Main.py
+++ Excel/Main.py
@@ -1,5 +1,10 @@
 import pandas
 import pandas as pd
 
-Data = pandas.read_excel('Develope.xlsx', sheet_name = "main")
-print(Data)
+Data = pandas.read_excel('System_Money.xlsx', sheet_name = "expend")
+
+Name = 'تفریح'
+
+for index, row in Data.iterrows():
+	if row['Block'] == Name:
+		print("block "+ row['Block']+ " Val "+ str(row['Val']) + " Data "+ row['Data'])
